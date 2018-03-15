960427e7f9f457ca2d35c4ea9a0b834797cddb8e
lichuang <lichuang1982@gmail.com>
2018-03-15 22:25:27 +0800

    ci

--- test/progress_test.cc
+++ test/progress_test.cc
@@ -5,23 +5,13 @@
 #include "default_logger.h"
 
 bool deepEqualInflights(const inflights& in1, const inflights& in2) {
-  if (in1.start_ != in2.start_) {
-    return false;
-  }
-  if (in1.count_ != in2.count_) {
-    return false;
-  }
-  if (in1.size_ != in2.size_) {
-    return false;
-  }
-  if (in1.buffer_.size() != in2.buffer_.size()) {
-    return false;
-  }
+  EXPECT_EQ(in1.start_, in2.start_);
+  EXPECT_EQ(in1.count_, in2.count_);
+  EXPECT_EQ(in1.size_, in2.size_);
+  EXPECT_EQ(in1.buffer_.size(), in2.buffer_.size());
   int i = 0;
   for (i = 0; i < in1.buffer_.size(); ++i) {
-    if (in1.buffer_[i] != in2.buffer_[i]) {
-      return false;
-    }
+    EXPECT_EQ(in1.buffer_[i], in2.buffer_[i]) << "i: " << i << ",in1:" << in1.buffer_[i] << ",in2:" << in2.buffer_[i];
   }
 
   return true;
@@ -40,16 +30,16 @@ TEST(progressTests, TestInflightsAdd) {
     wantIns.start_ = 0;
     wantIns.count_ = 5;
     wantIns.size_  = 10;
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(1);
-    wantIns.buffer_.push_back(2);
-    wantIns.buffer_.push_back(3);
-    wantIns.buffer_.push_back(4);
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(0);
+    wantIns.buffer_[0] = 0;
+    wantIns.buffer_[1] = 1;
+    wantIns.buffer_[2] = 2;
+    wantIns.buffer_[3] = 3;
+    wantIns.buffer_[4] = 4;
+    wantIns.buffer_[5] = 0;
+    wantIns.buffer_[6] = 0;
+    wantIns.buffer_[7] = 0;
+    wantIns.buffer_[8] = 0;
+    wantIns.buffer_[9] = 0;
     // ↓------------
     // 0, 1, 2, 3, 4, 0, 0, 0, 0, 0
     EXPECT_EQ(true, deepEqualInflights(ins, wantIns));
@@ -64,16 +54,16 @@ TEST(progressTests, TestInflightsAdd) {
     wantIns.start_ = 0;
     wantIns.count_ = 10;
     wantIns.size_  = 10;
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(1);
-    wantIns.buffer_.push_back(2);
-    wantIns.buffer_.push_back(3);
-    wantIns.buffer_.push_back(4);
-    wantIns.buffer_.push_back(5);
-    wantIns.buffer_.push_back(6);
-    wantIns.buffer_.push_back(7);
-    wantIns.buffer_.push_back(8);
-    wantIns.buffer_.push_back(9);
+    wantIns.buffer_[0] = 0;
+    wantIns.buffer_[1] = 1;
+    wantIns.buffer_[2] = 2;
+    wantIns.buffer_[3] = 3;
+    wantIns.buffer_[4] = 4;
+    wantIns.buffer_[5] = 5;
+    wantIns.buffer_[6] = 6;
+    wantIns.buffer_[7] = 7;
+    wantIns.buffer_[8] = 8;
+    wantIns.buffer_[9] = 9;
     // ↓--------------------------
     // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     EXPECT_EQ(true, deepEqualInflights(ins, wantIns));
@@ -82,9 +72,7 @@ TEST(progressTests, TestInflightsAdd) {
   // rotating case
   inflights ins2(10, &kDefaultLogger);
   ins2.start_ = 5;
-  ins2.size_ = 10;
 
-  return;
   for (i = 0; i < 5; ++i) {
     ins2.add(i);
   }
@@ -93,18 +81,40 @@ TEST(progressTests, TestInflightsAdd) {
     wantIns.start_ = 5;
     wantIns.count_ = 5;
     wantIns.size_  = 10;
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(0);
-    wantIns.buffer_.push_back(1);
-    wantIns.buffer_.push_back(2);
-    wantIns.buffer_.push_back(3);
-    wantIns.buffer_.push_back(4);
+    wantIns.buffer_[0] = 0;
+    wantIns.buffer_[1] = 0;
+    wantIns.buffer_[2] = 0;
+    wantIns.buffer_[3] = 0;
+    wantIns.buffer_[4] = 0;
+    wantIns.buffer_[5] = 0;
+    wantIns.buffer_[6] = 1;
+    wantIns.buffer_[7] = 2;
+    wantIns.buffer_[8] = 3;
+    wantIns.buffer_[9] = 4;
     //                ↓------------
     // 0, 0, 0, 0, 0, 0, 1, 2, 3, 4
+    EXPECT_EQ(true, deepEqualInflights(ins2, wantIns));
+  }
+  for (i = 5; i < 10; ++i) {
+    ins2.add(i);
+  }
+  {
+    inflights wantIns(10, &kDefaultLogger);
+    wantIns.start_ = 5;
+    wantIns.count_ = 10;
+    wantIns.size_  = 10;
+    wantIns.buffer_[0] = 5;
+    wantIns.buffer_[1] = 6;
+    wantIns.buffer_[2] = 7;
+    wantIns.buffer_[3] = 8;
+    wantIns.buffer_[4] = 9;
+    wantIns.buffer_[5] = 0;
+    wantIns.buffer_[6] = 1;
+    wantIns.buffer_[7] = 2;
+    wantIns.buffer_[8] = 3;
+    wantIns.buffer_[9] = 4;
+    // ---------------↓------------
+    // 5, 6, 7, 8, 9, 0, 1, 2, 3, 4
     EXPECT_EQ(true, deepEqualInflights(ins2, wantIns));
   }
 }
