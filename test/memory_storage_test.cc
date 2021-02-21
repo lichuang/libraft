997a8f426c4d815bb0478e99c5e26b1cb25c587c
lichuang <lichuang1982@gmail.com>
2021-02-21 17:29:05 +0800

    refactor test code

--- test/memory_storage_test.cc
+++ test/memory_storage_test.cc
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "libraft.h"
+#include "raft_test_util.h"
 #include "base/default_logger.h"
 #include "base/util.h"
 #include "storage/memory_storage.h"
@@ -11,40 +12,25 @@
 using namespace libraft;
 
 TEST(memoryStorageTests, TestStorageTerm) {
-  EntryVec entries;
-
-  {
-    Entry entry;
-
-    entry.set_index(3);
-    entry.set_term(3);
-    entries.push_back(entry);
-
-    entry.set_index(4);
-    entry.set_term(4);
-    entries.push_back(entry);
-
-    entry.set_index(5);
-    entry.set_term(5);
-    entries.push_back(entry);
-  }
+  EntryVec entries = {
+    initEntry(3,3),
+    initEntry(4,4),
+    initEntry(5,5),
+  };
 
   struct tmp {
     uint64_t i;
     int werr;
     uint64_t wterm;
-
-    tmp(uint64_t i, int err, uint64_t term)
-      : i(i), werr(err), wterm(term) {}
+  } tests[] = {
+    {.i = 2, .werr = ErrCompacted, .wterm = 0},
+    {.i = 3, .werr = OK, .wterm = 3},
+    {.i = 4, .werr = OK, .wterm = 4},
+    {.i = 5, .werr = OK, .wterm = 5},
+    {.i = 6, .werr = ErrUnavailable, .wterm = 0},
   };
-  vector<tmp> tests;
-  tests.push_back(tmp(2, ErrCompacted, 0));
-  tests.push_back(tmp(3, OK, 3));
-  tests.push_back(tmp(4, OK, 4));
-  tests.push_back(tmp(5, OK, 5));
-  tests.push_back(tmp(6, ErrUnavailable, 0));
   size_t i = 0;
-  for (i = 0; i < tests.size(); ++i) {
+  for (i = 0; i < SIZEOF_ARRAY(tests); ++i) {
     const tmp &test = tests[i];
     MemoryStorage s(&kDefaultLogger);
     s.entries_ = entries;
@@ -56,173 +42,66 @@ TEST(memoryStorageTests, TestStorageTerm) {
 }
 
 TEST(memoryStorageTests, TestStorageEntries) {
-  EntryVec entries;
-
-  {
-    Entry entry;
-
-    entry.set_index(3);
-    entry.set_term(3);
-    entries.push_back(entry);
-
-    entry.set_index(4);
-    entry.set_term(4);
-    entries.push_back(entry);
-
-    entry.set_index(5);
-    entry.set_term(5);
-    entries.push_back(entry);
-
-    entry.set_index(6);
-    entry.set_term(6);
-    entries.push_back(entry);
-  }
+  EntryVec entries = {
+    initEntry(3,3),
+    initEntry(4,4),
+    initEntry(5,5),
+    initEntry(6,6),
+  };
 
   struct tmp {
     uint64_t lo, hi, maxsize;
     int werr;
     EntryVec entries;
-
-    tmp(uint64_t lo, uint64_t hi, uint64_t maxsize, int err)
-      : lo(lo), hi(hi), maxsize(maxsize), werr(err) {}
-  };
-
-  vector<tmp> tests;
-  {
-    tests.push_back(tmp(2, 6, kNoLimit, ErrCompacted));
-    tests.push_back(tmp(3, 4, kNoLimit, ErrCompacted));
-
+  } tests[] = {
     {
-      Entry entry;
-
-      tmp t(4, 5, kNoLimit, OK);
-
-      entry.set_index(4);
-      entry.set_term(4);
-      t.entries.push_back(entry);
-      tests.push_back(t); 
-    }
-
+      .lo = 2, .hi = 6, .maxsize = kNoLimit, .werr = ErrCompacted,
+      .entries = {},
+    },
     {
-      Entry entry;
-
-      tmp t(4, 6, kNoLimit, OK);
-
-      entry.set_index(4);
-      entry.set_term(4);
-      t.entries.push_back(entry);
-
-      entry.set_index(5);
-      entry.set_term(5);
-      t.entries.push_back(entry);
-
-      tests.push_back(t); 
-    }
-
+      .lo = 3, .hi = 4, .maxsize = kNoLimit, .werr = ErrCompacted,
+      .entries = {},
+    }, 
     {
-      Entry entry;
-
-      tmp t(4, 7, kNoLimit, OK);
-
-      entry.set_index(4);
-      entry.set_term(4);
-      t.entries.push_back(entry);
-
-      entry.set_index(5);
-      entry.set_term(5);
-      t.entries.push_back(entry);
-
-      entry.set_index(6);
-      entry.set_term(6);
-      t.entries.push_back(entry);
-      tests.push_back(t); 
-    }
+      .lo = 4, .hi = 5, .maxsize = kNoLimit, .werr = OK,
+      .entries = {initEntry(4,4)},
+    },        
+    {
+      .lo = 4, .hi = 6, .maxsize = kNoLimit, .werr = OK,
+      .entries = {initEntry(4,4), initEntry(5,5)},
+    }, 
+    {
+      .lo = 4, .hi = 7, .maxsize = kNoLimit, .werr = OK,
+      .entries = {initEntry(4,4), initEntry(5,5), initEntry(6,6)},
+    },         
     // even if maxsize is zero, the first entry should be returned
     {
-      Entry entry;
-
-      tmp t(4, 7, 0, OK);
-
-      entry.set_index(4);
-      entry.set_term(4);
-      t.entries.push_back(entry);
-
-      tests.push_back(t); 
-    }
+      .lo = 4, .hi = 7, .maxsize = 0, .werr = OK,
+      .entries = {initEntry(4,4),},
+    },   
     // limit to 2
     {
-      Entry entry;
-
-      int size = entries[1].ByteSizeLong() + entries[2].ByteSizeLong();
-      tmp t(4, 7, size, OK);
-
-      entry.set_index(4);
-      entry.set_term(4);
-      t.entries.push_back(entry);
-
-      entry.set_index(5);
-      entry.set_term(5);
-      t.entries.push_back(entry);
-
-      tests.push_back(t); 
-    }
+      .lo = 4, .hi = 7, .maxsize = entries[1].ByteSizeLong() + entries[2].ByteSizeLong(), .werr = OK,
+      .entries = {initEntry(4,4),initEntry(5,5),},
+    },     
     // limit to 2
     {
-      Entry entry;
-
-      int size = entries[1].ByteSizeLong() + entries[2].ByteSizeLong() + entries[3].ByteSizeLong() / 2;
-      tmp t(4, 7, size, OK);
-
-      entry.set_index(4);
-      entry.set_term(4);
-      t.entries.push_back(entry);
-
-      entry.set_index(5);
-      entry.set_term(5);
-      t.entries.push_back(entry);
-
-      tests.push_back(t); 
-    }
+      .lo = 4, .hi = 7, .maxsize = entries[1].ByteSizeLong() + entries[2].ByteSizeLong() + entries[3].ByteSizeLong() / 2, .werr = OK,
+      .entries = {initEntry(4,4),initEntry(5,5),},
+    },   
     {
-      Entry entry;
-
-      int size = entries[1].ByteSizeLong() + entries[2].ByteSizeLong() + entries[3].ByteSizeLong() - 1;
-      tmp t(4, 7, size, OK);
-
-      entry.set_index(4);
-      entry.set_term(4);
-      t.entries.push_back(entry);
-
-      entry.set_index(5);
-      entry.set_term(5);
-      t.entries.push_back(entry);
-
-      tests.push_back(t); 
-    }
-    // all
+      .lo = 4, .hi = 7, .maxsize = entries[1].ByteSizeLong() + entries[2].ByteSizeLong() + entries[3].ByteSizeLong() - 1, .werr = OK,
+      .entries = {initEntry(4,4),initEntry(5,5),},
+    },  
+    // all   
     {
-      Entry entry;
+      .lo = 4, .hi = 7, .maxsize = entries[1].ByteSizeLong() + entries[2].ByteSizeLong() + entries[3].ByteSizeLong(), .werr = OK,
+      .entries = {initEntry(4,4),initEntry(5,5),initEntry(6,6)},
+    },     
+  };
 
-      int size = entries[1].ByteSizeLong() + entries[2].ByteSizeLong() + entries[3].ByteSizeLong();
-      tmp t(4, 7, size, OK);
-
-      entry.set_index(4);
-      entry.set_term(4);
-      t.entries.push_back(entry);
-
-      entry.set_index(5);
-      entry.set_term(5);
-      t.entries.push_back(entry);
-
-      entry.set_index(6);
-      entry.set_term(6);
-      t.entries.push_back(entry);
-
-      tests.push_back(t); 
-    }
-  }
   size_t i = 0;
-  for (i = 0; i < tests.size(); ++i) {
+  for (i = 0; i < SIZEOF_ARRAY(tests); ++i) {
     const tmp &test = tests[i];
     MemoryStorage s(&kDefaultLogger);
     EntryVec ret;
@@ -235,23 +114,11 @@ TEST(memoryStorageTests, TestStorageEntries) {
 }
 
 TEST(memoryStorageTests, TestStorageLastIndex) {
-  EntryVec entries;
-
-  {
-    Entry entry;
-
-    entry.set_index(3);
-    entry.set_term(3);
-    entries.push_back(entry);
-
-    entry.set_index(4);
-    entry.set_term(4);
-    entries.push_back(entry);
-
-    entry.set_index(5);
-    entry.set_term(5);
-    entries.push_back(entry);
-  }
+  EntryVec entries = {
+    initEntry(3,3),
+    initEntry(4,4),
+    initEntry(5,5),
+  };
 
   MemoryStorage s(&kDefaultLogger);
   s.entries_ = entries;
@@ -261,15 +128,7 @@ TEST(memoryStorageTests, TestStorageLastIndex) {
   EXPECT_EQ(OK, err);
   EXPECT_EQ((int)last, 5);
 
-  {
-    EntryVec tmp_entries;
-    Entry entry;
-
-    entry.set_index(6);
-    entry.set_term(5);
-    tmp_entries.push_back(entry);
-    s.Append(tmp_entries);
-  }
+  s.Append(EntryVec({initEntry(6,5)}));
 
   err = s.LastIndex(&last);
   EXPECT_EQ(OK, err);
@@ -277,23 +136,11 @@ TEST(memoryStorageTests, TestStorageLastIndex) {
 }
 
 TEST(memoryStorageTests, TestStorageFirstIndex) {
-  EntryVec entries;
-
-  {
-    Entry entry;
-
-    entry.set_index(3);
-    entry.set_term(3);
-    entries.push_back(entry);
-
-    entry.set_index(4);
-    entry.set_term(4);
-    entries.push_back(entry);
-
-    entry.set_index(5);
-    entry.set_term(5);
-    entries.push_back(entry);
-  }
+  EntryVec entries = {
+    initEntry(3,3),
+    initEntry(4,4),
+    initEntry(5,5),
+  };
 
   MemoryStorage s(&kDefaultLogger);
   s.entries_ = entries;
@@ -318,23 +165,11 @@ TEST(memoryStorageTests, TestStorageFirstIndex) {
 }
 
 TEST(memoryStorageTests, TestStorageCompact) {
-  EntryVec entries;
-
-  {
-    Entry entry;
-
-    entry.set_index(3);
-    entry.set_term(3);
-    entries.push_back(entry);
-
-    entry.set_index(4);
-    entry.set_term(4);
-    entries.push_back(entry);
-
-    entry.set_index(5);
-    entry.set_term(5);
-    entries.push_back(entry);
-  }
+  EntryVec entries = {
+    initEntry(3,3),
+    initEntry(4,4),
+    initEntry(5,5),
+  };
 
   MemoryStorage s(&kDefaultLogger);
   s.entries_ = entries;
@@ -345,17 +180,15 @@ TEST(memoryStorageTests, TestStorageCompact) {
     uint64_t wterm;
     uint64_t windex;
     int wlen;
-
-    tmp(uint64_t i, int err, uint64_t index, uint64_t term, int len)
-      : i(i), werr(err), wterm(term), windex(index), wlen(len) {}
+  } tests[] = {
+    { .i = 2, .werr = ErrCompacted, .wterm = 3, .windex = 3, .wlen = 3, },
+    { .i = 3, .werr = ErrCompacted, .wterm = 3, .windex = 3, .wlen = 3, },
+    { .i = 4, .werr = OK, .wterm = 4, .windex = 4, .wlen = 2, },
+    { .i = 5, .werr = OK, .wterm = 5, .windex = 5, .wlen = 1, },
   };
-  vector<tmp> tests;
-  tests.push_back(tmp(2, ErrCompacted, 3, 3, 3));
-  tests.push_back(tmp(3, ErrCompacted, 3, 3, 3));
-  tests.push_back(tmp(4, OK, 4, 4, 2));
-  tests.push_back(tmp(5, OK, 5, 5, 1));
+
   size_t i = 0;
-  for (i = 0; i < tests.size(); ++i) {
+  for (i = 0; i < SIZEOF_ARRAY(tests); ++i) {
     const tmp &test = tests[i];
     MemoryStorage tmp_s(&kDefaultLogger);
     tmp_s.entries_ = entries;
