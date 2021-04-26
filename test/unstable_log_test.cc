a9e5873e05102cf7fbd3cfe9c24061e72ea90ac9
lichuang <lichuang1982@gmail.com>
2021-04-26 19:27:25 +0800

    ci

--- test/unstable_log_test.cc
+++ test/unstable_log_test.cc
@@ -54,6 +54,7 @@ TEST(unstableLogTests, TestUnstableMaybeFirstIndex) {
     uint64_t index;
     bool ok = unstable.maybeFirstIndex(&index);
     EXPECT_EQ(tests[i].wok, ok);
+    EXPECT_EQ(tests[i].windex, index);
     if (tests[i].snapshot != NULL) {
       delete tests[i].snapshot;
     }
